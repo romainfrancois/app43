8cf0dc7a3bf9263eb3b6302745baf762de90a719
Romain Francois <romain@tada.science>
2026-09-17 10:04:18 +0200

    update

--- app/app.R
+++ app/app.R
@@ -1,27 +1,32 @@
 library(shiny)
 library(bslib)
+library(reticulate)
 
 ui <- page_fluid(
-  sidebarLayout(
-    sidebarPanel(
-      actionButton("go", "Go: 40 + 2"),
-    ),
-    mainPanel(
-      verbatimTextOutput("summary")
-    )
-  )
+  h1("checks"),
+  verbatimTextOutput("summary"),
+  h1("run python code"),
+  actionButton("go", "Run '40 + 2' in python"),
+  verbatimTextOutput("py")
 )
 
 server <- function(input, output, session) {
 
-  py <- eventReactive(input$go, {
-    reticulate::py_eval("40 + 2")
+  output$summary <- renderText({
+    paste(
+      paste("reticulate version:", utils::packageVersion("reticulate")),
+      paste("python available:", py_available(initialize = TRUE)),
+      sep = "\n"
+    )
   })
 
-  output$summary <- renderText({
+  py <- eventReactive(input$go, {
+    py_eval("40 + 2")
+  })
+
+  output$py <- renderText({
     py()
   })
-
 }
 
 shinyApp(ui, server)
